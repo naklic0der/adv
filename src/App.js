8d06d2db92858e8ca7c6ec7886656ad621d0433c
naklic0der <naklicoder@gmail.com>
2020-12-14 21:53:23 +0530

    install compatible sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './App.module.sass';
+import style from './App.module.sass'
 
 function App() {
    return (
-      <div className="App">
+      <div className={style.App}>
          <p>Hume koi farak nahi padta, dekho ye sab toh kismat ka khel hai</p>
       </div>
    );
