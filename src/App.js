b1c0fddc07fe7dfcf3252d07854292d838229b30
naklic0der <naklicoder@gmail.com>
2020-12-14 23:06:48 +0530

    Installed MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import style from './App.module.sass'
+import Button from '@material-ui/core/Button';
 
 function App() {
    return (
       <div className={style.App}>
          <p>Hume koi farak nahi padta, dekho ye sab toh kismat ka khel hai</p>
+         <Button variant="contained" color="primary">
+            Hello World
+         </Button>
       </div>
    );
 }
