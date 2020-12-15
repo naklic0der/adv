4209303fe61d5ec77d59b861bb31bb5b73fefc18
naklic0der <naklicoder@gmail.com>
2020-12-15 18:26:01 +0530

    stuff updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
 import style from './App.module.sass'
 import Button from '@material-ui/core/Button';
 import Block from "./Components/Block";
+import React, {useEffect} from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+
+   useEffect(() => {
+      AOS.init();
+      AOS.refresh();
+   }, []);
+
    return (
       <div className={style.App}>
          <p>Hume koi acha or kya acha wese abhi?? ka khel hai</p>
@@ -11,10 +19,10 @@ function App() {
             Click me !!
          </Button>
          <div className={style.test}>
-            <Block data-aos={"fade-in"}>one</Block>
-            <Block data-aos={"fade-in"}>two</Block>
-            <Block data-aos={"fade-in"}>three</Block>
-            <Block data-aos={"fade-in"}>four</Block>
+            <Block anim="fade-up">one</Block>
+            <Block anim="fade-up">two</Block>
+            <Block anim="fade-left">three</Block>
+            <Block anim="fade-right">four</Block>
          </div>
       </div>
    );
--- src/Components/Block.js
+++ src/Components/Block.js
@@ -4,7 +4,7 @@ import style from './Block.module.sass'
 const Block = (props) => {
 
    return (
-      <div className={style.Block}>
+      <div className={style.Block} data-aos={props.anim}>
          {props.children}
       </div>
    )
